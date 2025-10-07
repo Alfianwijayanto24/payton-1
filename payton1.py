91329b3ac88ade87557c3afb6e5367df5d4c7602
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-10-07 15:29:29 +0700

    memasukan persentasi nilai siswa

---
+++ payton1.py
@@ -0,0 +1,11 @@
+percentege = float(input("Memasukan persentase nilai siswa:"))
+if percentege >=90:
+    print("Excellent performance")
+elif percentege >= 80:
+    print ("very Good performance")
+elif percentege >= 70:
+    print ("Good performance")
+elif percentege >= 60:
+    print ("Average performance")
+else:
+    print ("Need improvement")
