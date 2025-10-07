bab5293dbad6ae7c0a9d0e9ee325093a2b9030fd
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-10-07 15:28:41 +0700

    memasukan angkaa

---
+++ prak2.py
@@ -0,0 +1,15 @@
+a = float(input("Masukan angka pertama:"))
+b = float(input("Masukan angka kedua :"))
+c = float(input("Masukan angka ketiga:"))
+
+if a > b and a > c:
+    largest = a
+elif b > a and b > c:
+    largest = b
+elif c > a and c > b:
+    largest = c
+else:
+    largest = print("tidak ada angka terbesar")
+
+    print("Angka tersebar adalah:",largest)
+
